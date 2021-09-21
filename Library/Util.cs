2b2d881185e6bb321c0756495d047052b32d49a0
Vinicius Martins <vinicius.martins@bludata.com.br>
2021-09-21 08:16:17 -0300

    First commit

---
+++ Library/Util.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Library
+{
+    public static class Util
+    {
+        public static void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static string Append(string text)
+        {
+            return $"Hello {text}";
+        }
+    }
+}
