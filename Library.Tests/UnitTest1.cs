c374cfc0ecbba066807c30acc07977b3a0d6541c
Vinicius Martins <vinicius.martins@bludata.com.br>
2021-09-22 16:27:28 -0300

    Arrumados testes

--- Library.Tests/UnitTest1.cs
+++ Library.Tests/UnitTest1.cs
@@ -19,7 +19,7 @@ namespace Library.Tests
         [Test]
         public void Append_TestTwo()
         {
-            Assert.That(Util.Append("Teste") == "Hello");
+            Assert.That(Util.Append("Teste") == "Hello Teste");
         }
     }
 }
