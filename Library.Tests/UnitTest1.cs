b91ad26c745965a829a2f49aa25e4241e64c0551
Vinicius Martins <vinicius.martins@bludata.com.br>
2021-09-22 16:17:51 -0300

    Adicionados testes automatizados

---
+++ Library.Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+
+namespace Library.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Append_TestOne()
+        {
+            Assert.That(Util.Append("Teste") == "Hello Teste");
+        }
+
+        [Test]
+        public void Append_TestTwo()
+        {
+            Assert.That(Util.Append("Teste") == "Hello");
+        }
+    }
+}
